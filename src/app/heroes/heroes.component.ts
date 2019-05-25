51efec265355180c5c7e3da821a3cd054fe4cd2d
Cyril Cesco <cyril.cesco@gmail.com>
2019-05-25 22:45:12 +0200

    etape services, observable, subscribe et master/detail components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,34 @@
+/* tslint:disable:semicolon */
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { Observable, of } from 'rxjs';
+import {HEROES} from '../mock-heroes';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, public messageService: MessageService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.messageService.add('clic sur : ' + hero.name);
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
